597b4a9ca83fddc4a418088bea722c4628e4c7e0
Liliya-Tazieva <tazieva2107@gmail.com>
2018-03-05 00:24:28 +0300

    Changed something in dijkstra flood fill class

--- Main part Unity/Assets/Scripts/PathFinding/DijkstraFloodFill.cs
+++ Main part Unity/Assets/Scripts/PathFinding/DijkstraFloodFill.cs
@@ -38,6 +38,186 @@ namespace Assets.Scripts.PathFinding
             return CurrentIteration;
         }
 
+        private Tree_Node AddRight(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y()].Currentnode.InformerNode.IsObstacle 
+                ? NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y()] : null;
+        }
+
+        private Tree_Node AddLeft(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y()].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y()] : null;
+        }
+
+        private Tree_Node AddUp(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X(), current.Currentnode.Y() + 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X(), current.Currentnode.Y() + 1] : null;
+        }
+
+        private Tree_Node AddDown(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X(), current.Currentnode.Y() - 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X(), current.Currentnode.Y() - 1] : null;
+        }
+
+        private Tree_Node AddUpRight(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y() + 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y() + 1]
+                : null;
+        }
+
+        private Tree_Node AddUpLeft(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y() + 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y() + 1]
+                : null;
+        }
+
+        private Tree_Node AddDownRight(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y() - 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X() + 1, current.Currentnode.Y() - 1]
+                : null;
+        }
+
+        private Tree_Node AddDownLeft(Tree_Node current)
+        {
+            return !NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y() - 1].Currentnode.InformerNode.IsObstacle
+                ? NodesArray[current.Currentnode.X() - 1, current.Currentnode.Y() - 1]
+                : null;
+        }
+
+        public List<Tree_Node> AddAll(Tree_Node current)
+        {
+            var nodes = new List<Tree_Node>();
+            nodes.Add(AddDown(current));
+            nodes.Add(AddDownLeft(current));
+            nodes.Add(AddDownRight(current));
+            nodes.Add(AddLeft(current));
+            nodes.Add(AddUp(current));
+            nodes.Add(AddRight(current));
+            nodes.Add(AddUpLeft(current));
+            nodes.Add(AddUpRight(current));
+            return nodes;
+        }
+
+        public List<Tree_Node> Neighbours(Tree_Node current)
+        {
+            var neighbours = new List<Tree_Node>();
+            if (current.Currentnode.DestinationFromStart == Destinations.Default)
+            {
+                neighbours.AddRange(AddAll(current));
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.Right)
+            {
+                var x = current.Currentnode.X();
+                var y = current.Currentnode.Y();
+                var node = AddRight(current);
+                neighbours.Add(node);
+                var isForced = false;
+
+                if (NodesArray[x, y + 1].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddUp(node));
+                    neighbours.Add(AddUpRight(node));
+                    isForced = true;
+                }
+                if (NodesArray[x, y - 1].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddDown(node));
+                    neighbours.Add(AddDownRight(node));
+                    isForced = true;
+                }
+                if(isForced) neighbours.Add(AddRight(node));
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.Left)
+            {
+                var x = current.Currentnode.X();
+                var y = current.Currentnode.Y();
+                var node = AddLeft(current);
+                neighbours.Add(node);
+                var isForced = false;
+
+                if (NodesArray[x, y + 1].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddUp(node));
+                    neighbours.Add(AddUpLeft(node));
+                    isForced = true;
+                }
+                if (NodesArray[x, y - 1].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddDown(node));
+                    neighbours.Add(AddDownLeft(node));
+                    isForced = true;
+                }
+                if (isForced) neighbours.Add(AddLeft(node));
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.Up)
+            {
+                var x = current.Currentnode.X();
+                var y = current.Currentnode.Y();
+                var node = AddUp(current);
+                neighbours.Add(node);
+                var isForced = false;
+
+                if (NodesArray[x + 1, y].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddRight(node));
+                    neighbours.Add(AddUpRight(node));
+                    isForced = true;
+                }
+                if (NodesArray[x - 1, y].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddLeft(node));
+                    neighbours.Add(AddUpLeft(node));
+                    isForced = true;
+                }
+                if (isForced) neighbours.Add(AddUp(node));
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.Down)
+            {
+                var x = current.Currentnode.X();
+                var y = current.Currentnode.Y();
+                var node = AddDown(current);
+                neighbours.Add(node);
+                var isForced = false;
+
+                if (NodesArray[x + 1, y].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddRight(node));
+                    neighbours.Add(AddDownRight(node));
+                    isForced = true;
+                }
+                if (NodesArray[x - 1, y].Currentnode.InformerNode.IsObstacle)
+                {
+                    neighbours.Add(AddLeft(node));
+                    neighbours.Add(AddDownLeft(node));
+                    isForced = true;
+                }
+                if (isForced) neighbours.Add(AddDown(node));
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.UpRight)
+            {
+
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.UpLeft)
+            {
+
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.DownRight)
+            {
+
+            }
+            else if (current.Currentnode.DestinationFromStart == Destinations.DownLeft)
+            {
+
+            }
+            return neighbours;
+        }
+
         public void Flood(int startRow, int startCol)
         {
             CurrentIteration++;
@@ -55,7 +235,7 @@ namespace Assets.Scripts.PathFinding
                 // This must be in the search style of JPS+ in order to produce
                 // the correct data for JPS+. If goal bounding is used for regular
                 // A*, then this search would need to mimic regular A*.
-                var newSuccessors = Extensions.NeighboursForGB(currentNode, NodesArray, null);
+                var newSuccessors = Neighbours(currentNode);
                 foreach (var successor in newSuccessors)
                 {
                     if (!OpenList.Exists(arg => arg.Currentnode.Position == successor.Currentnode.Position)
